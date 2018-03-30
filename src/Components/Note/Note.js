22ffc4a6bd66e7cf06d1fcda615bbc397dda4460
Daniel Słaby <pufek93@gmail.com>
2018-03-30 10:17:10 +0200

    Wire hover effect on Note

--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -4,28 +4,54 @@ import styled from 'styled-components';
 import { Card } from 'antd';
 const { Meta } = Card;
 
-export const Note = ({ className, title, body }) => {
-	return (
-		<Draggable
-			axis="both"
-			handle=".handle"
-			defaultPosition={{ x: 0, y: 0 }}
-			position={null}
-			grid={[25, 25]}
-			onStart={this.handleStart}
-			onDrag={this.handleDrag}
-			onStop={this.handleStop}
-		>
-			<Card className={`${className} handle`}>
-				<Meta title={title} description={body} />
-			</Card>
-		</Draggable>
-	);
-};
+export class Note extends React.PureComponent {
+	constructor() {
+		super();
+		this.state = {
+			isHovered: false
+		};
+	}
+
+	onMouseOver = () => {
+		this.setState({
+			isHovered: true
+		});
+	};
+
+	onMouseLeave = () => {
+		this.setState({
+			isHovered: false
+		});
+	};
+
+	render() {
+		const { className, title, body } = this.props;
+		return (
+			<Draggable
+				axis="both"
+				handle=".handle"
+				defaultPosition={{ x: 0, y: 0 }}
+				position={null}
+				grid={[25, 25]}
+				onStart={this.handleStart}
+				onDrag={this.handleDrag}
+				onStop={this.handleStop}
+			>
+				<Card onMouseOver={this.onMouseOver} onMouseLeave={this.onMouseLeave} className={`${className} handle`}>
+					<Meta title={title} description={body} />
+				</Card>
+			</Draggable>
+		);
+	}
+}
 
 const StyledNote = styled(Note)`
 	width: 230px;
 	height: 250px;
+
+	&:hover {
+		box-shadow: 0px 0px 10px 0px #a7a7a7;
+	}
 `;
 
 export default StyledNote;
